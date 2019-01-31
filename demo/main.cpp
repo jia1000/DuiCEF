04359ee2b25d8e393c6cfe82af56b055ea703a41
jia1000 <jiajiajiajia10006@163.com>
2019-01-31 16:55:15 +0800

    jia1000_duicef版本：调整了资源文件的存放路径；以及修改了exe的生成路径。只保留一份资源文件。

--- demo/main.cpp
+++ demo/main.cpp
@@ -17,6 +17,68 @@ CWindowWnd* CreateMainWnd(LPCTSTR pstrName)
 	return mainFrame;
 }
 
+void InitResource()
+{
+    // 资源类型
+
+    DuiLib::CPaintManagerUI::SetResourceType(DuiLib::UILIB_FILE);
+    //DuiLib::CPaintManagerUI::SetResourceType(DuiLib::UILIB_ZIPRESOURCE);
+
+    // 资源路径
+    DuiLib::CDuiString strResourcePath = DuiLib::CPaintManagerUI::GetInstancePath();
+    // 加载资源
+    switch(DuiLib::CPaintManagerUI::GetResourceType())
+    {
+    case DuiLib::UILIB_FILE:
+        {
+            strResourcePath += _T("skin\\");
+            DuiLib::CPaintManagerUI::SetResourcePath(strResourcePath.GetData());
+            // 加载资源管理器
+            DuiLib::CResourceManager::GetInstance()->LoadResource(_T("res.xml"), NULL);
+            break;
+        }
+    case DuiLib::UILIB_RESOURCE:
+        {
+            strResourcePath += _T("skin\\");
+            DuiLib::CPaintManagerUI::SetResourcePath(strResourcePath.GetData());
+            // 加载资源管理器
+            DuiLib::CResourceManager::GetInstance()->LoadResource(_T("IDR_RES"), _T("xml"));
+            break;
+        }
+    case DuiLib::UILIB_ZIP:
+        {
+            strResourcePath += _T("skin\\");
+            DuiLib::CPaintManagerUI::SetResourcePath(strResourcePath.GetData());
+            // 加密
+            //CPaintManagerUI::SetResourceZip(_T("XXXXXX.zip"), true, _T("XXXXXXX"));
+            //CPaintManagerUI::SetResourceZip(_T("XXXXXX.zip"), true);
+            // 加载资源管理器
+            DuiLib::CResourceManager::GetInstance()->LoadResource(_T("res.xml"), NULL);
+            break;
+        }
+    case DuiLib::UILIB_ZIPRESOURCE:
+        {
+            strResourcePath += _T("skin\\");
+            DuiLib::CPaintManagerUI::SetResourcePath(strResourcePath.GetData());
+            HRSRC hResource = ::FindResource(DuiLib::CPaintManagerUI::GetResourceDll(), _T("IDR_ZIPRES"), _T("ZIPRES"));
+            if( hResource != NULL ) {
+                DWORD dwSize = 0;
+                HGLOBAL hGlobal = ::LoadResource(DuiLib::CPaintManagerUI::GetResourceDll(), hResource);
+                if( hGlobal != NULL ) {
+                    dwSize = ::SizeofResource(DuiLib::CPaintManagerUI::GetResourceDll(), hResource);
+                    if( dwSize > 0 ) {
+                        DuiLib::CPaintManagerUI::SetResourceZip((LPBYTE)::LockResource(hGlobal), dwSize);
+                        // 加载资源管理器
+                        DuiLib::CResourceManager::GetInstance()->LoadResource(_T("res.xml"), NULL);
+                    }
+                }
+                ::FreeResource(hResource);
+            }
+        }
+        break;
+    }
+}
+
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPTSTR    lpCmdLine,
@@ -34,7 +96,10 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 
 	// ------------------duilib初始化------------------
 	CPaintManagerUI::SetInstance(hInstance);
-	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath() + _T("Skin\\"));
+    // 初始化资源
+    InitResource();
+	//CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath() + _T("Skin\\"));
+
 	if (FAILED(::CoInitialize(NULL)))
 	{
 		return -1;
